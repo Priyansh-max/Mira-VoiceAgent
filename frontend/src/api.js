31c0e305af14012cbc7689c1a3c73ca68247e3e4
Priyansh-max <priyanshagarwal3381@gmail.com>
2026-03-07 03:46:13 +0530

    production_backend url suppport through env file

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,4 @@
-const API = 'http://127.0.0.1:8000';
+const API = import.meta.env.VITE_BACKEND_URL
 
 export async function createSession() {
   const res = await fetch(`${API}/sessions`, { method: 'POST' });
